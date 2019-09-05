ee0b420e3df96f3e94ab478e670576fb157af79d
Joel Holland <Dnallohleoj@users.noreply.github.com>
2019-09-04 22:40:48 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# agileShooter
+
+Game project that combines the need to aim and be accurate of Fortnite and the hack-and-slash acrobatics of a Devil May Cry or Kingdom Hearts
+
+Developed with Unreal Engine 4
---
+++ Source/agileShooter/agileShooter.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "agileShooter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, agileShooter, "agileShooter" );
